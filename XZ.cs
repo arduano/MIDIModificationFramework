6715e5f47783ef2152a64878cd28eb9402b2bff4
arduano <leonid.shchurov@gmail.com>
2019-04-19 22:41:57 +1000

    streams

--- XZ.cs
+++ XZ.cs
@@ -8,6 +8,75 @@ using System.Threading.Tasks;
 
 namespace MIDIModificationFramework
 {
+    public class XZStream : Stream
+    {
+        Process xz;
+        Task copyTask;
+        public XZStream(Stream write, bool autoClose = true, int threads = 0)
+        {
+            xz = new Process();
+            xz.StartInfo = new ProcessStartInfo("xz", "-zc --threads=" + threads)
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false
+            };
+            xz.Start();
+            copyTask = Task.Run(() =>
+            {
+                xz.StandardOutput.BaseStream.CopyTo(write);
+                if (autoClose) write.Close();
+            });
+            stdin = xz.StandardInput.BaseStream;
+            //xz.StandardInput.Close();
+        }
+
+        public override bool CanRead => false;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => true;
+
+        public override long Length => throw new NotImplementedException();
+
+        public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        Stream stdin;
+
+        public override void Flush()
+        {
+            stdin.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            stdin.Write(buffer, offset, count);
+        }
+
+        public override void Close()
+        {
+            xz.StandardInput.Close();
+            copyTask.GetAwaiter().GetResult();
+            //xz.WaitForExit();
+            base.Close();
+        }
+    }
+
     public static class XZ
     {
         public static Stream RemoveXZCompressionLayer(Stream input, int threads = 0)
